fc3cdcbbc5c41553b848fd2c64a4e0cc4162ba35
Abhishek Singh <abhishek.singh@shoogloo.com>
2024-12-19 12:28:06 +0530

    Commit

--- app/_header/Header.jsx
+++ app/_header/Header.jsx
@@ -10,6 +10,8 @@ const Header = () => {
   const [cartCount, setCartCount] = useState(0);
   const [menuOpen, setMenuOpen] = useState(false);
   const [subMenu , setSubMenu] = useState(false);
+  const [activeCategory, setActiveCategory] = useState(null);
+
   useEffect(() => {
     setCartCount(cart.items.length);
   }, [cart]);
@@ -17,45 +19,47 @@ const Header = () => {
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
-
+  const toggleSubmenu = (category) => {
+    setActiveCategory(activeCategory === category ? null : category);
+  };
   return (
     <div className={styles.headerContainer}>
       <div className={styles.header}>
-        {
-       !menuOpen  && <Link href='/'>
-          <img src='/images/astro369logo1.png' alt='Logo' className={styles.logo} />
-        </Link>
-        }
       {
-       !menuOpen && <div className={styles.hamburger} onClick={toggleMenu}>
+       <div className={styles.hamburger} onClick={toggleMenu}>
           <img src='/images/hamburger.png' alt='Menu' />
         </div>
         }
-        <div className={`${styles.rightContainerDiv} ${menuOpen ? styles.showMenu : ''}`}>
+        {
+  <Link href='/'>
+          <img src='/images/astro369logo1.png' alt='Logo' className={styles.logo} />
+        </Link>
+        }
+    
+        <div className={`${styles.rightContainerDiv} `}>
         <div style={{display:'flex' , flexDirection:'row-reverse',justifyContent:'space-between' }}>
-       {
-         menuOpen&&<p  onClick={toggleMenu} style={{fontWeight:'bold', float:'right'}}>X</p>
-       }
+       
         <Search />
         </div>
          <div className={styles.cartContainer}>
-         <Link href='/'>
-            <img src='/images/wishList.png' alt='Wish List' className={styles.addCart} height={40} width={40} />
-          </Link>
+        {  <Link href='/'>
+            <img src='/images/wishList.png' alt='Wish List' className={`${styles.addCart} ${styles.wishList}` } height={40} width={40} />
+          </Link>}
           <Link href='/cart'>
             <div className={styles.cartNotification}>
               <img src='/images/addToCart.png' alt='Add to cart' className={styles.addCart} height={40} width={40} />
               <span className={styles.cartCount}>{cartCount}</span>
             </div>
           </Link>
-          <Link href='#' onMouseEnter={()=>{setSubMenu(true)}} onMouseLeave={()=>{setSubMenu(false)}}>
+          <Link href='#' onClick={()=>{setSubMenu(!subMenu);}}>
             <img src='/images/user.png' alt='User' className={styles.addCart} height={40} width={40} />
           </Link>
+          
          </div>
         </div>
       </div>
       {
-      subMenu && <div className={styles.subMenu}  onMouseEnter={()=>{setSubMenu(true)}} onMouseLeave={()=>{setSubMenu(false)}}>
+     ( subMenu) && <div className={styles.subMenu}  onMouseEnter={()=>{setSubMenu(true)}} onMouseLeave={()=>{setSubMenu(false)}}>
         <ul>
         <Link href='/login'><li>Login/Register</li></Link>
           <li>My Acount</li>
@@ -64,6 +68,51 @@ const Header = () => {
         </ul>
          </div>
          }
+
+{menuOpen  && (
+        <div className={styles.menu}>
+          <div className={styles.menuItem} onClick={() => toggleSubmenu('men')}>
+            <p>Men </p><p className={styles.arrow}>{activeCategory === 'men' ? '>' : 'v'}</p>
+          </div>
+          {activeCategory === 'men' && (
+            <div className={styles.submenu}>
+              <a href="#men-shirts">Shirts</a>
+              <a href="#men-pants">Pants</a>
+              <a href="#men-shoes">Shoes</a>
+            </div>
+          )}
+          <div className={styles.menuItem} onClick={() => toggleSubmenu('women')}>
+           <p> Women</p> <p className={styles.arrow}>{activeCategory === 'women' ? '>' : 'v'}</p>
+          </div>
+          {activeCategory === 'women' && (
+            <div className={styles.submenu}>
+              <a href="#women-dresses">Dresses</a>
+              <a href="#women-tops">Tops</a>
+              <a href="#women-shoes">Shoes</a>
+            </div>
+          )}
+          <div className={styles.menuItem} onClick={() => toggleSubmenu('kids')}>
+           <p> Kids</p> <p className={styles.arrow}>{activeCategory === 'kids' ? '>' : 'v'}</p>
+          </div>
+          {activeCategory === 'kids' && (
+            <div className={styles.submenu}>
+              <a href="#kids-boys">Boys</a>
+              <a href="#kids-girls">Girls</a>
+              <a href="#kids-shoes">Shoes</a>
+            </div>
+          )}
+          <div className={styles.menuItem} onClick={() => toggleSubmenu('sale')}>
+           <p>Sale</p>  <p className={styles.arrow}>{activeCategory === 'sale' ? '>' : 'v'}</p>
+          </div>
+          {activeCategory === 'sale' && (
+            <div className={styles.submenu}>
+              <a href="#sale-men">Men</a>
+              <a href="#sale-women">Women</a>
+              <a href="#sale-kids">Kids</a>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
